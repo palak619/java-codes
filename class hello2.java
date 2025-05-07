41576a86416fa48f0ea1a95a72179b0956953eb3
palak619 <tpalak931@gmail.com>
2025-05-07 10:05:39 +0530

    Add files via upload

---
+++ class hello2.java
@@ -0,0 +1,16 @@
+
+import java.lang.reflect.Array;
+
+class hello2
+{
+  int a=5;
+  public static void main (String [] args){
+     hello2 obj =new hello2();
+     System.out.println(obj.a);
+
+  }
+}
+    for ( i=0; i<Arraylength; i++)
+    for (j=)
+
+
