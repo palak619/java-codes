b1275cf8bd2c85a5c669f60a3dee42c9675a9dab
palak619 <tpalak931@gmail.com>
2025-05-07 10:03:13 +0530

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,32 @@
+public class Good {
+    public static void main(String []arg){
+        int a=7;
+
+        switch(a){
+
+              case 1:
+            System.out.println(" monday");
+                break;
+                case 2: 
+                System.out.println( "tuesday");
+                break;
+                case 3: 
+                System.out.println(" wend");
+                break;
+                case 4:
+                 System.out.println( "thr");
+                break;
+                case 5:
+                 System.out.println( "stru");
+                break;
+                case 6:
+                 System.out.println("st");
+                break;
+                case 7:
+                 System.out.println("sund");
+                break;
+                 default:
+                  System.out.println("kuch ni");
+                }
+}
+    }
