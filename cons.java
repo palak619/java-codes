9a6c911eaa3ca73ca899da78cf57317f3d9d627e
palak619 <tpalak931@gmail.com>
2025-05-07 10:10:29 +0530

    Add files via upload

---
+++ cons.java
@@ -0,0 +1,20 @@
+class student{
+    String firstName= "palak";
+     int age =11;{
+          student(String firstName,int age);{
+           this.firstName=firstName;
+              age=age;
+          }
+          System.out.println (firstName+"  "age);
+     }
+   }
+      class hello {
+        public static void main(String[] args) {
+          student obj= new Student(palak,11);
+          student obj1= new Student(pooja,17);
+        }
+
+              
+             
+      }     
+            
