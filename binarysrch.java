86afce83a1861923f14f45dd2a962859eab2e578
palak619 <tpalak931@gmail.com>
2025-05-07 10:10:56 +0530

    Add files via upload

---
+++ binarysrch.java
@@ -0,0 +1,23 @@
+
+import java.lang.reflect.Array;
+
+class binarysrch1{
+        public static int  {
+            int target=2;
+            int start=0;
+            int endpoint= Array.length-1;{
+                   while{
+                       start<= endpoint};
+                        int mid point = (start+end point)/2;
+                          if 
+                        
+                              
+                          }
+                   } 
+                       
+                   }
+            }
+            
+        
+         
+}
