c759691c098b4a020894b32fa2e0431b05faf191
palak <tpalak931@gmail.com>
2025-05-09 18:38:39 +0530

    Add files via upload

---
+++ modulus opertor.java
@@ -0,0 +1,9 @@
+public class ModulusExample {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 3;
+        int remainder = num1 % num2; // 10 divided by 3 gives remainder 1
+
+        System.out.println("Remainder: " + remainder); // Output: Remainder: 1
+    }
+}
